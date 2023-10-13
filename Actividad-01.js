2dd9283081d2ebd4a846c20eead9632cfa834067
Sergi <121488207+SergiMPorto@users.noreply.github.com>
2023-10-13 14:22:16 +0200

    Update Actividad-01.js
    Añadiendo eventos opciones

--- Actividad-01.js
+++ Actividad-01.js
@@ -1,11 +1,82 @@
-//Eventos
+document.addEventListener("DOMContentLoaded", () => {
+    const opciones = document.getElementsByClassName("opcion");
 
-document.addEventListener("DocumentLoader",()=> {
+    for (const opcion of opciones) {
+        opcion.addEventListener('mouseover', () => {
+            opcion.style.width = '200px';
+            opcion.style.height = '200px';
+            opcion.style.borderColor = 'red';
+        });
 
-    const piedra = document.getElementById('piedra');
+        opcion.addEventListener('mouseout', () => {
+            opcion.style.width = '135px';
+            opcion.style.height = '101px';
+            opcion.style.borderColor = 'initial';
+        });
+        opcion.addEventListener('click', () => {
+            opcion.style.width = '200px';
+            opcion.style.height = '200px';
+            opcion.style.borderColor = 'red';
+        });
 
-    
-    piedra.addEventListener('click', () => {
-        piedra.style.borderColor = 'red'; 
-    });
+    }
 });
+
+let jugador;
+let maquina;
+let piedram;
+let piedra;
+let tijera;
+let tijeram;
+let papel;
+let papelm;
+let lagarto;
+let lagartom;
+let spoock;
+let spoockm;
+let jugadaJugador;
+let jugadaMaquina;
+
+function iniciarVariable() {
+    jugador = document.getElementsByClassName("jugador");
+    maquina = document.getElementsByClassName("maquina");
+    piedra = document.getElementById("piedra");
+    piedram = document.getElementById("piedram");
+    tijera = document.getElementById("tijera");
+    tijeram = document.getElementById("tijeram");
+    papel = document.getElementById("papel");
+    papelm = document.getElementById("papelm");
+    lagarto = document.getElementById("lagarto");
+    lagartom = document.getElementById("lagartom");
+    spoock = document.getElementById("spoock");
+    spoockm = document.getElementById("spoockm");
+}
+
+function juegoJugador(eleccion) {
+    jugadaJugador = eleccion;
+}
+
+function juegoMaquina() {
+    const opciones = ["piedram", "papelm", "tijeram", "lagartom", "spoockm"];
+    jugadaMaquina = opciones[Math.floor(Math.random() * opciones.length)];
+}
+
+function jugar() {
+    if (jugadaJugador === jugadaMaquina) {
+        alert("Empate");
+    } else if (
+        (jugadaJugador === "piedra" && jugadaMaquina === "tijeram") ||
+        (jugadaJugador === "papel" && jugadaMaquina === "piedram") ||
+        (jugadaJugador === "papel" && jugadaMaquina === "spoockm") ||
+        (jugadaJugador === "lagarto" && jugadaMaquina === "spoockm") ||
+        (jugadaJugador === "spoock" && jugadaMaquina === "tijeram") ||
+        (jugadaJugador === "tijera" && jugadaMaquina === "lagartom") ||
+        (jugadaJugador === "lagarto" && jugadaMaquina === "papelm") ||
+        (jugadaJugador === "spoock" && jugadaMaquina === "piedram")
+    ) {
+        alert("Ganador");
+    } else {
+        alert("Perdedor");
+    }
+}
+
